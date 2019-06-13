dd096654cf0a05e729d9547fa9d342c468004999
Phoenix <117663444@qq.com>
2019-06-13 11:05:32 +0800

    任务6：请求（Request）

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('posts')
 export class PostsController {
   @Get()
-  index() {
+  index(@Req() request: Request) {
+    console.log(
+      request.ip,
+      request.hostname,
+      request.method
+    );
+    
     return [
       {
         title: 'hello ~'
