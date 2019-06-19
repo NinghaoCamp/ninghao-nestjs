884948e1972e08bfc635371cf96c991722ad0812
Phoenix <117663444@qq.com>
2019-06-19 16:06:03 +0800

    任务4：拦截器：在处理器之前或之后要做的事情

--- src/core/interceptors/logging.interceptor.ts
+++ src/core/interceptors/logging.interceptor.ts
@@ -1,10 +1,17 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const now = Date.now();
+    console.log('before...');
     console.log(`I'm a interceptor.`);
-    return next.handle();
+    return next
+      .handle()
+      .pipe(
+        tap(() => console.log(`after... ${Date.now() - now}ms`))
+      );
   }
 }
