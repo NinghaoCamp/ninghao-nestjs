6ea054c8b5a6c9ef443dd666f5d4db4fc1feb1e8
Phoenix <117663444@qq.com>
2019-06-23 11:19:00 +0800

    任务5：安装与配置 JWT 模块（@nestjs/jwt）

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secretOrPrivateKey: '1AGy4bCUoECDZ4yI6h8DxHDwgj84EqStMNyab8nPChQ=',
+      signOptions: {
+        expiresIn: '12h'
+      }
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
