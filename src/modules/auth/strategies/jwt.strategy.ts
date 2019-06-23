1c17ecf57c8d59b0844a28f692988bfceb4f47cb
Phoenix <117663444@qq.com>
2019-06-23 11:44:26 +0800

    任务10：验证身份的方法（validate）

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,11 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt, VerifiedCallback } from 'passport-jwt';
 import { JwtPayload } from '../auth.interface';
+import { UserService } from '../../user/user.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(
+    private readonly userService: UserService
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: '1AGy4bCUoECDZ4yI6h8DxHDwgj84EqStMNyab8nPChQ='
@@ -14,6 +17,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: JwtPayload, done: VerifiedCallback) {
     console.log('payload: ', payload);
+    const { name } = payload;
+    const entity = await this.userService.findByName(name);
 
+    if (!entity) {
+      // done(new UnauthorizedException('没找到用户。'));
+      throw new UnauthorizedException('没找到用户。');
+    }
+
+    // done(null, entity);
+    return entity;
   }
 }
