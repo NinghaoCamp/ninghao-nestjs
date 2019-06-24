0bb96ad5a042c3f2f1033d0a787143ebc59bb439
Phoenix <117663444@qq.com>
2019-06-24 13:20:56 +0800

    任务3：在 Post 模块中使用 AuthGuard 验证用户身份

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -3,12 +3,14 @@ import { PostController } from './post.controller';
 import { PostService } from './post.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './post.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     TypeOrmModule.forFeature([Post])
   ],
   controllers: [PostController],
   providers: [PostService]
 })
-export class PostModule {}
+export class PostModule { }
