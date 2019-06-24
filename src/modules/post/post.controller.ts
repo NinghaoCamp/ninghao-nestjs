125c1166e895d9bd69fc07d1faf1639bab2d082f
Phoenix <117663444@qq.com>
2019-06-24 14:02:17 +0800

    任务8：用 ParseIntPipe 把参数值的类型转换成数字

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, Post, Get, Param, Put, Delete, UseGuards, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+import { Controller, Body, Post, Get, Param, Put, Delete, UseGuards, UseInterceptors, ClassSerializerInterceptor, ParseIntPipe } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostDto } from './post.dto';
 import { AuthGuard } from '@nestjs/passport';
@@ -36,5 +36,10 @@ export class PostController {
   @Delete(':id')
   async destroy(@Param('id') id: string) {
     return await this.postService.destroy(id);
+  }
+
+  @Post(':id/vote')
+  async vote(@Param('id', ParseIntPipe) id: number) {
+    console.log(typeof id);
   }
 }
