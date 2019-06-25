cf867d56ea05e717c32b791cb58d65d07ca96e26
Phoenix <117663444@qq.com>
2019-06-25 18:00:28 +0800

    任务7：定义分类实体与内容实体的关系

--- src/modules/category/category.entity.ts
+++ src/modules/category/category.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Post } from '../post/post.entity';
 
 @Entity()
 export class Category {
@@ -10,4 +11,7 @@ export class Category {
 
   @Column()
   alias: string;
+
+  @OneToMany(type => Post, post => post.category)
+  posts: Post[];
 }
