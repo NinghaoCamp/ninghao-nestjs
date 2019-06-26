ee515d16156cf68b5c457b83d620426adf3af0b7
Phoenix <117663444@qq.com>
2019-06-26 20:50:11 +0800

    任务5：存储内容评论

--- src/modules/comment/comment.module.ts
+++ src/modules/comment/comment.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { Comment } from './comment.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Comment]),
+    AuthModule
+  ],
   controllers: [CommentController],
   providers: [CommentService]
 })
-export class CommentModule {}
+export class CommentModule { }
--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Comment } from './comment.entity';
+import { Repository } from 'typeorm';
+import { User } from '../user/user.entity';
+import { CommentDto } from './comment.dto';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(
+    @InjectRepository(Comment)
+    private readonly commentRepository: Repository<Comment>
+  ) { }
+
+  async storePostComment(id: number, user: User, data: CommentDto) {
+    return await this.commentRepository.save({
+      user,
+      ...data,
+      post: { id }
+    })
+  }
+}
