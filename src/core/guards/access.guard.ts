45e733033a56e47adf30ce26ee8a9b550842b1f2
Phoenix <117663444@qq.com>
2019-06-29 18:02:28 +0800

    任务11：在权限控制守卫检查用户角色

--- src/core/guards/access.guard.ts
+++ src/core/guards/access.guard.ts
@@ -1,14 +1,47 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { PermissionInterface } from '../interfaces/permissions.interface';
+import { User } from '../../modules/user/user.entity';
 
 @Injectable()
 export class AccessGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
-    console.log(request.user);
+  constructor(
+    private readonly reflector: Reflector
+  ) { }
 
-    return false;
+  async validatePermissions(
+    permissions: PermissionInterface[],
+    user: User
+  ) {
+    const results = permissions.map(async permission => {
+      const { role } = permission;
+      let hasRole: boolean;
+
+      if (role) {
+        hasRole = user.roles.some(userRole => userRole.name === role);
+      }
+
+      return hasRole;
+    });
+
+    return Promise.all(results);
+  }
+
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const permissions = this.reflector.get(
+      'permissions',
+      context.getHandler()
+    );
+
+    const results = await this.validatePermissions(
+      permissions,
+      request.user
+    );
+
+    return results.includes(true);
   }
 }
